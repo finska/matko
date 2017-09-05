465cbeab919df235d660b63a723186973a498d98
Srdjan Sin Jovanovic <mp4065@gmail.com>
2017-09-05 02:11:37 +0200

    reverted back to plain old html, imported Bootstrap 4 and removed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826222047) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "events", force: :cascade do |t|
-    t.integer "code_id"
-    t.datetime "time"
-    t.text "status"
-    t.text "place"
-    t.boolean "notified", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "shipments", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'mp4065@gmail.com')
+Shipment.create(code: '373325381117900560')
+Event.create(time: '25.07.2017, 16:38', status: 'Handed over to the
+recipient', place: 'OULU')
+Event.create(time: '20.07.2017, 10:09', status: 'Ready to be collected', place: 'K-MARKET OULU HÖNTTÄMÄKI/T:MI KM TASKILA, RUOTUTIE 3, OULU')
